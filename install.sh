847ef7edfc085dc5f6dfbd18e83ab5fdedace0d8
Alex <alexzhangs@gmail.com>
2019-06-21 09:52:05 +0800

    add installation and service start scripts

---
+++ install.sh
@@ -0,0 +1,116 @@
+#!/bin/bash
+
+# Exit on any error
+set -o pipefail -e
+
+usage () {
+    printf "Usage: ${0##*/} [-d DOMAIN] [-i IP] [-o PORT] [-u USERNAME] [-p PASSWORD] [-e EMAIL] [-t TIMEZONE]\n"
+    printf "Run this script under root on Linux.\n"
+    printf "OPTIONS\n"
+    printf "\t[-d DOMAIN]\n\n"
+    printf "\tDomain name resolved to shadowsocks-manager web application.\n\n"
+    printf "\t[-i IP]\n\n"
+    printf "\tIP address bound to shadowsocks-manager web application, default is 127.0.0.1.\n\n"
+    printf "\t[-o PORT]\n\n"
+    printf "\tPort number bound to shadowsocks-manager web application, default is 8000.\n\n"
+    printf "\t[-u USERNAME]\n\n"
+    printf "\tUsername for shadowsocks-manager administrator, default is 'admin'.\n\n"
+    printf "\t[-p PASSWORD]\n\n"
+    printf "\tPassword for shadowsocks-manager administrator, default is 'passw0rd'.\n\n"
+    printf "\t[-e EMAIL]\n\n"
+    printf "\tEmail for shadowsocks-manager administrator.\n\n"
+    printf "\t[-t TIMEZONE]\n\n"
+    printf "\tDefault is 'UTC'.\n\n"
+    printf "\t[-h]\n\n"
+    printf "\tThis help.\n\n"
+    exit 255
+}
+
+while getopts d:i:o:u:p:e:t:h opt; do
+    case $opt in
+        d)
+            DOMAIN=$OPTARG
+            ;;
+        i)
+            IP=$OPTARG
+            ;;
+        o)
+            PORT=$OPTARG
+            ;;
+        u)
+            USERNAME=$OPTARG
+            ;;
+        p)
+            PASSWORD=$OPTARG
+            ;;
+        e)
+            EMAIL=$OPTARG
+            ;;
+        t)
+            TIMEZONE=$OPTARG
+            ;;
+        *|h)
+            usage
+            ;;
+    esac
+done
+
+[[ -z $IP ]] && IP=127.0.0.1
+[[ -z $PORT ]] && PORT=8000
+[[ -z $USERNAME ]] && USERNAME=admin
+[[ -z $PASSWORD ]] && PASSWORD=passw0rd
+[[ -z $TIMEZONE ]] && TIMEZONE=UTC
+
+WORKDIR=${0%/*}
+
+if [[ $(uname) != 'Linux' ]]; then
+    printf "Error: this script support Linux only.\n" >&2
+    exit 9
+fi
+
+printf "Installing python dependencies...\n"
+pip install -r "$WORKDIR/requirements.txt"
+
+static_dir="/var/local/www/$DOMAIN/static/"
+
+printf "Creating static dir: $static_dir...\n"
+mkdir -p "$static_dir"
+
+function guid () {
+    od  -vN "$(($1 / 2))" -An -tx1 /dev/urandom | tr -d ' \n'
+}
+
+printf "Modifying Django settings...\n"
+secret_key=$(guid 50)
+setting_file="$WORKDIR/shadowsocks_manager/shadowsocks_manager/settings.py"
+sed -e "s/DEBUG = True/DEBUG = False/" \
+    -e "s/ALLOWED_HOSTS = .*/ALLOWED_HOSTS = ['$IP', '$DOMAIN']/" \
+    -e "s|STATIC_ROOT = .*|STATIC_ROOT = '$static_dir'|" \
+    -e "s/SECRET_KEY = .*/SECRET_KEY = '$secret_key'/" \
+    -e "s/TIME_ZONE = .*/TIME_ZONE = '$TIMEZONE'/" \
+    -i "$setting_file"
+
+printf "Load Django fixtures...\n"
+cd "$WORKDIR/shadowsocks_manager"
+python manage.py makemigrations
+python manage.py migrate
+python manage.py loaddata auth.group.json \
+       django_celery_beat.crontabschedule.json \
+       django_celery_beat.intervalschedule.json \
+       django_celery_beat.periodictask.json \
+       config.json \
+       template.json
+
+printf "Creating super user...\n"
+echo "from django.contrib.auth.models import User; User.objects.filter(username='$USERNAME').delete(); User.objects.create_superuser('$USERNAME', '$EMAIL', '$PASSWORD')" \
+    | python manage.py shell
+
+printf "Collecting static files...\n"
+python manage.py collectstatic --no-input -c
+
+printf "Copying and patching init.d scripts...\n"
+cp -a "$WORKDIR/shadowsocks-manager-web" /etc/init.d/
+sed -i "s/LISTEN_ADDRESS=.*/LISTEN_ADDRESS=$IP:$PORT/" /etc/init.d/shadowsocks-manager-web
+cp -a "$WORKDIR/shadowsocks-manager-job" /etc/init.d/
+
+exit
--- shadowsocks_manager/shadowsocks_manager/settings.py
+++ shadowsocks_manager/shadowsocks_manager/settings.py
@@ -124,6 +124,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = ''
 
 
 # Django REST Framework
