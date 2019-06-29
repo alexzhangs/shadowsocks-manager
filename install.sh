b075de684e25947b6eddcb1c7b865776f2191024
Alex <alexzhangs@gmail.com>
2019-06-29 12:11:31 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,15 +4,13 @@
 set -o pipefail -e
 
 usage () {
-    printf "Usage: ${0##*/} [-n DOMAIN] [-a IP] [-o PORT] [-u USERNAME] [-p PASSWORD] [-e EMAIL] [-t TIMEZONE]\n"
+    printf "Usage: ${0##*/} [-n DOMAIN] [-a IP] [- USERNAME] [-p PASSWORD] [-e EMAIL] [-t TIMEZONE]\n"
     printf "Run this script under root on Linux.\n"
     printf "OPTIONS\n"
     printf "\t[-n DOMAIN]\n\n"
     printf "\tDomain name resolved to shadowsocks-manager web application.\n\n"
     printf "\t[-a IP]\n\n"
     printf "\tIP address bound to shadowsocks-manager web application, default is 127.0.0.1.\n\n"
-    printf "\t[-o PORT]\n\n"
-    printf "\tPort number bound to shadowsocks-manager web application, default is 8000.\n\n"
     printf "\t[-u USERNAME]\n\n"
     printf "\tUsername for shadowsocks-manager administrator, default is 'admin'.\n\n"
     printf "\t[-p PASSWORD]\n\n"
@@ -26,16 +24,13 @@ usage () {
     exit 255
 }
 
-while getopts n:a:o:u:p:e:t:h opt; do
+while getopts n:a:u:p:e:t:h opt; do
     case $opt in
         n)
             DOMAIN=$OPTARG
             ;;
         a)
             IP=$OPTARG
-            ;;
-        o)
-            PORT=$OPTARG
             ;;
         u)
             USERNAME=$OPTARG
@@ -56,7 +51,6 @@ while getopts n:a:o:u:p:e:t:h opt; do
 done
 
 [[ -z $IP ]] && IP=127.0.0.1
-[[ -z $PORT ]] && PORT=8000
 [[ -z $USERNAME ]] && USERNAME=admin
 [[ -z $PASSWORD ]] && PASSWORD=passw0rd
 [[ -z $TIMEZONE ]] && TIMEZONE=UTC
@@ -69,7 +63,7 @@ if [[ $(uname) != 'Linux' ]]; then
 fi
 
 RUN_AS=ssm
-INSTALL_DIR=/home/$RUN_AS
+INSTALL_DIR=/home/$RUN_AS/shadowsocks-manager
 
 printf "Creating user $RUN_AS...\n"
 useradd $RUN_AS
@@ -77,15 +71,24 @@ useradd $RUN_AS
 printf "Copying project files to $INSTALL_DIR...\n"
 su - $RUN_AS -c "cp -a $WORK_DIR $INSTALL_DIR"
 
+printf "Changing to install directory...\n"
+cd "$INSTALL_DIR"
+
 printf "Installing python dependencies...\n"
-pip install -r "$INSTALL_DIR/requirements.txt"
+pip install -r requirements.txt
+
+printf "Modifying nginx conf files...\n"
+sed -i "s/{DOMAIN}/${DOMAIN}/" nginx/conf.d/shadowsocks-manager.conf
+
+printf "Copying nginx conf files...\n"
+cp -a nginx/* /etc/nginx/
+
+printf "Copying supervisor program conf files...\n"
+cp -a supervisor/* /etc/supervisor/conf.d/
 
 STATIC_DIR="/var/local/www/$DOMAIN/static/"
 printf "Creating static dir: $STATIC_DIR...\n"
 mkdir -p "$STATIC_DIR"
-
-printf "Modifying nginx conf files...\n"
-sed -i "s/{DOMAIN}/${DOMAIN}/" $INSTALL_DIR/nginx/conf.d/shadowsocks-manager.conf
 
 function guid () {
     od -vN "$(($1 / 2))" -An -tx1 /dev/urandom | tr -d ' \n'
@@ -93,7 +96,7 @@ function guid () {
 
 printf "Modifying Django settings...\n"
 secret_key=$(guid 50)
-setting_file="$INSTALL_DIR/shadowsocks_manager/shadowsocks_manager/settings.py"
+setting_file="$INSTALL_DIR/shadowsocks_manager/settings.py"
 sed -e "s/DEBUG = True/DEBUG = False/" \
     -e "s/ALLOWED_HOSTS = .*/ALLOWED_HOSTS = ['$IP', '$DOMAIN']/" \
     -e "s|STATIC_ROOT = .*|STATIC_ROOT = '$STATIC_DIR'|" \
@@ -120,11 +123,5 @@ echo "from django.contrib.auth.models import User; User.objects.filter(username=
 
 printf "Collecting static files...\n"
 python manage.py collectstatic --no-input -c
-
-printf "Copying nginx conf files...\n"
-cp -a $INSTALL_DIR/nginx/* /etc/nginx/
-
-printf "Copying supervisor program conf files...\n"
-cp -a $INSTALL_DIR/supervisor/* /etc/supervisor/conf.d/
 
 exit
