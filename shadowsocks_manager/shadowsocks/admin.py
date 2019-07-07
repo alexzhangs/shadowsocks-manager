269849bfd37d28b93537590ef6c39580d7846426
Alex <alexzhangs@gmail.com>
2019-07-07 23:56:42 +0800

    Update admin.py
    add 'password' to Account import/export field list

--- shadowsocks_manager/shadowsocks/admin.py
+++ shadowsocks_manager/shadowsocks/admin.py
@@ -136,7 +136,7 @@ class AccountResource(resources.ModelResource):
     
     class Meta:
         model = Account
-        fields = ('id', 'username', 'email', 'first_name', 'last_name', 'date_joined',)
+        fields = ('id', 'username', 'password', 'email', 'first_name', 'last_name', 'date_joined',)
         export_order = ('username',)
 
 
