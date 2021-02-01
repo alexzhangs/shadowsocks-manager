e0e92aace89d769b3386c33247f3a39cf97c6ea6
Alex <alexzhangs@gmail.com>
2021-02-02 00:24:37 +0800

    add the support to Amazon Linux 2 AMI.
    the changes from Amazon Linux 2 AMI:
    * the yum repo epel is only available after to run amazon-linux-extras.
    * pip is no longer shipped with default.
    * the default installation directory prefix is change from /usr/local to /usr, for the new installed python packages.

--- install-dependency.sh
+++ install-dependency.sh
@@ -1,12 +1,51 @@
 #!/bin/bash
 
+## Install the external packages depended by shadowsocks-manager.
+## Below packages are neccessary to run shadowsocks-manager.
+##
+##   * rabbitmq-server
+##   * memecached
+##   * pip
+##
+
 # Exit on any error
 set -o pipefail -e
 
+if [[ $(uname) != 'Linux' ]]; then
+    printf "Error: this script support Linux only.\n" >&2
+    exit 9
+fi
+
+# install pip if missing
+# Amazon Linux 2 AMI needs this
+if ! type pip >/dev/null 2>&1; then
+    python_version=$(python --version 2>&1)
+    python_version=${version:7:1}
+
+    if [[ $python_version -eq 2 ]]; then
+        # We can't install the latest version of pip here but to a version < 21.
+        # pip dropped support for Python 2.7 since Jan, 2021. Amazon Linux and Linux 2
+        # AMIs are still using Python 2.7.
+        curl https://bootstrap.pypa.io/2.7/get-pip.py -o get-pip.py
+    else
+        # Get the latest version of pip.
+        curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
+    fi
+    python get-pip.py
+fi
+
+# install yum repo: repl
+# Amazon Linux 2 AMI needs this
+if type amazon-linux-extras >/dev/null 2>&1; then
+    amazon-linux-extras install -y epel
+fi
+
+# rabbitmq-server
 yum install -y rabbitmq-server --enablerepo=epel
 chkconfig rabbitmq-server on
 service rabbitmq-server start
 
+# memcached
 yum install -y memcached
 chkconfig memcached on
 service memcached start
--- install-extra.sh
+++ install-extra.sh
@@ -1,17 +1,34 @@
 #!/bin/bash
 
+## Install the extra packages for shadowsocks-manager.
+## Below packages are recommended for the production deployment.
+## 
+##   * nginx (web server)
+##   * uwsgi (application server)
+##   * gcc (for building uwsgi)
+##   * supervisor (to supervise the process of uwsgi and celery)
+##
+
 # Exit on any error
 set -o pipefail -e
 
+if [[ $(uname) != 'Linux' ]]; then
+    printf "Error: this script support Linux only.\n" >&2
+    exit 9
+fi
+
+# nginx
 yum install -y nginx
 chkconfig nginx on
 nginx -t && service nginx start
 
+# supervisor
 git clone --depth 1 https://github.com/alexzhangs/aws-ec2-supervisor
 bash aws-ec2-supervisor/aws-ec2-supervisor-install.sh -i -v 4.0.3
 chkconfig supervisord on
 service supervisord start
 
+# gcc and uwsgi
 yum install -y gcc
 pip install uwsgi==2.0.18
 
