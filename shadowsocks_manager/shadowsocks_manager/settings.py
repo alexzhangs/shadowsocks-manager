617931027a4eb023016f2b5c647f7189669770eb
Alex <alexzhangs@gmail.com>
2021-03-17 21:23:44 +0800

    Update settings.py
    add test database

--- shadowsocks_manager/shadowsocks_manager/settings.py
+++ shadowsocks_manager/shadowsocks_manager/settings.py
@@ -106,6 +106,9 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'TEST': {
+            'NAME': 'db-test',
+        },
     }
 }
 
