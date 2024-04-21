ebdc24a76f06d0d562915526faa8771646130cd7
Alex <alexzhangs@gmail.com>
2024-04-22 03:06:14 +0800

    Refactor test cases and disable logging for cleaner output

--- shadowsocks_manager/domain/tests.py
+++ shadowsocks_manager/domain/tests.py
@@ -5,18 +5,81 @@ from __future__ import unicode_literals
 from __future__ import absolute_import
 
 import json
+from abc import abstractmethod
 from django.test import TestCase
 
 from domain import models, serializers
 
 
-# Create your tests here.
-class DomainTestCase(TestCase):
-    fixtures = ['nameserver.json']
+import logging
+# disable logging for the test module to make the output clean
+logging.disable(logging.CRITICAL)
+
+
+class BaseTestCase(TestCase):
+    """
+    A base test case for current app.
+    This class is inherited from Django TestCase class, and provides the following features:
+
+    Set a list of default fixtures for all test cases in this app.
+    * fixtures = []
+
+    Set a list of test case class names, if the up() and down() class methods are defined in
+    the sub-class, all of them can be called at once by calling the allup() and alldown() class 
+    methods in this class.
+    * testcases = []
+    """
+
+    fixtures = []
+    testcases = []
+
+    class meta:
+        abstract = True
 
     @classmethod
+    @abstractmethod
     def up(cls):
-        print('Domain.NameServer: loading data ...')
+        """
+        Set up the test environment.
+        """
+        pass
+
+    @classmethod
+    @abstractmethod
+    def down(cls):
+        """
+        Tear down the test environment.
+        """
+        pass
+
+    @classmethod
+    def allup(cls):
+        """
+        Call the up() class method (if exists) in each class defined in the testcases list.
+        """
+        for testcase in [globals().get(name) for name in cls.testcases]:
+            method = getattr(testcase or {}, 'up', None)
+            if method: method()
+
+    @classmethod
+    def alldown(cls):
+        """
+        Call the down() class method (if exists) in each class defined in the testcases list.
+        """
+        for testcase in [globals().get(name) for name in cls.testcases]:
+            method = getattr(testcase or {}, 'down', None)
+            if method: method()
+
+
+class AppTestCase(BaseTestCase):
+    fixtures = ['nameserver.json']
+    testcases = ['NameServerTestCase', 'DomainTestCase', 'RecordTestCase']
+
+
+class NameServerTestCase(AppTestCase):
+    @classmethod
+    def up(cls):
+        # add mock username and credential to nameserver
         for obj in models.NameServer.objects.all():
             if not obj.user:
                 obj.user = 'mock-user'
@@ -24,45 +87,52 @@ class DomainTestCase(TestCase):
                 obj.credential = 'mock-credential'
             obj.save()
 
-        print('Domain.Domain: loading data ...')
+    @classmethod
+    def setUpTestData(cls):
+        cls.allup()
+
+    def test_nameserver_is_api_accessible_negative(self):
+        for obj in models.NameServer.objects.all():
+            self.assertFalse(obj.is_api_accessible)
+
+    def test_nameserver_serializer(self):
+        obj = serializers.NameServerSerializer()
+        json.loads(json.dumps(obj.to_representation(models.NameServer.objects.first())))
+
+
+class DomainTestCase(AppTestCase):
+    @classmethod
+    def up(cls):
+        # add mock domain
         for ns in models.NameServer.objects.all():
             obj = models.Domain(name='mock-example-{}.com'.format(ns.pk), nameserver=ns)
             obj.save()
 
-        print('Domain.Record: loading data ...')
-        for domain in models.Domain.objects.all():
-            obj = models.Record(host='ss', domain=domain, type='A', answer='1.1.1.1')
-            obj.save()
-
-    @classmethod
-    def down(cls):
-        print('Domain: tearing down ...')
-        pass
-
     @classmethod
     def setUpTestData(cls):
-        cls.up()
+        cls.allup()
 
-    def test_nameserver(self):
-        for obj in models.NameServer.objects.all():
-            self.assertTrue(str(obj))
-            self.assertFalse(obj.is_api_accessible)
-
-        obj = serializers.NameServerSerializer()
-        json.loads(json.dumps(obj.to_representation(models.NameServer.objects.first())))
-
-    def test_domain(self):
-        for obj in models.Domain.objects.all():
-            self.assertTrue(str(obj))
-
+    def test_domain_serializer(self):
         obj = serializers.DomainSerializer()
         json.loads(json.dumps(obj.to_representation(models.Domain.objects.first())))
 
-    def test_record(self):
+
+class RecordTestCase(AppTestCase):
+    @classmethod
+    def up(cls):
+        # add mock record
+        for domain in models.Domain.objects.all():
+            obj = models.Record(host='vpn', domain=domain, type='A', answer='1.1.1.1')
+            obj.save()
+
+    @classmethod
+    def setUpTestData(cls):
+        cls.allup()
+
+    def test_record_is_matching_dns_query_negative(self):
+        for obj in models.Record.objects.all():
+            self.assertFalse(obj.is_matching_dns_query)
+
+    def test_record_serializer(self):
         obj = serializers.RecordSerializer()
         json.loads(json.dumps(obj.to_representation(models.Record.objects.first())))
-
-        for obj in models.Record.objects.all():
-            self.assertTrue(str(obj))
-            self.assertFalse(obj.is_matching_dns_query)
-            obj.delete()
--- shadowsocks_manager/statistic/tests.py
+++ shadowsocks_manager/statistic/tests.py
@@ -5,33 +5,95 @@ from __future__ import unicode_literals
 from __future__ import absolute_import
 
 import json
+from abc import abstractmethod
 from django.test import TestCase
 
-from shadowsocksz.tests import AllData as ShadowsocksTestData
+from shadowsocksz.tests import AppTestCase as ShadowsockszAppTestCase
 from statistic import models, serializers
 
-# Create your tests here.
-class PeriodTestCase(TestCase):
 
-    def test(self):
+import logging
+# disable logging for the test module to make the output clean
+logging.disable(logging.CRITICAL)
+
+
+class BaseTestCase(TestCase):
+    """
+    A base test case for current app.
+    This class is inherited from Django TestCase class, and provides the following features:
+
+    Set a list of default fixtures for all test cases in this app.
+    * fixtures = []
+
+    Set a list of test case class names, if the up() and down() class methods are defined in
+    the sub-class, all of them can be called at once by calling the allup() and alldown() class 
+    methods in this class.
+    * testcases = []
+    """
+
+    fixtures = []
+    testcases = []
+
+    class meta:
+        abstract = True
+
+    @classmethod
+    @abstractmethod
+    def up(cls):
+        """
+        Set up the test environment.
+        """
+        pass
+
+    @classmethod
+    @abstractmethod
+    def down(cls):
+        """
+        Tear down the test environment.
+        """
+        pass
+
+    @classmethod
+    def allup(cls):
+        """
+        Call the up() class method (if exists) in each class defined in the testcases list.
+        """
+        for testcase in [globals().get(name) for name in cls.testcases]:
+            method = getattr(testcase or {}, 'up', None)
+            if method: method()
+
+    @classmethod
+    def alldown(cls):
+        """
+        Call the down() class method (if exists) in each class defined in the testcases list.
+        """
+        for testcase in [globals().get(name) for name in cls.testcases]:
+            method = getattr(testcase or {}, 'down', None)
+            if method: method()
+
+
+class AppTestCase(BaseTestCase):
+    fixtures = ShadowsockszAppTestCase.fixtures
+    testcases = []
+
+
+class PeriodTestCase(AppTestCase):
+    def test_period_serializer(self):
         obj = serializers.PeriodSerializer()
         json.loads(json.dumps(obj.to_representation(models.Period.objects.first())))
 
 
-class StatisticTestCase(TestCase):
-    fixtures = []
-    fixtures.extend(ShadowsocksTestData.fixtures)
+class StatisticTestCase(AppTestCase):
+    @classmethod
+    def setUpTestData(cls):
+        ShadowsockszAppTestCase.allup()
 
-    def setUp(self):
-        ShadowsocksTestData.up()
-
-    def tearDown(self):
-        ShadowsocksTestData.down()
-
-    def test(self):
+    def test_statistic_statistic(self):
         # no statistics data is asserted, cause there's no Manager API really called.
         models.Statistic.statistic()
-        models.Statistic.reset()
 
         obj = serializers.StatisticSerializer()
         json.loads(json.dumps(obj.to_representation(models.Statistic.objects.first())))
+
+    def test_statistic_reset(self):
+        models.Statistic.reset()
